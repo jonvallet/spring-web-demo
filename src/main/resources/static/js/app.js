da1a4265ef8e9dbe27de40060f0cad2fd7397850
Jon Vallet <jon.vallet@proxama.com>
2015-05-18 13:37:33 +0100

    Implemented real calls to the rest service

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,9 +4,16 @@ app.controller('appCtrl', function ($scope, $http) {
 
 
     $http.get('/appointments/list').success(function(data) {
-        $scope.appointments = data
+        $scope.appointments = data.content
     })
 
-    $scope.appointments = {"content":[{"description":"lol","date":1335205543511},{"description":"lol","date":1335205543511}],"last":true,"totalPages":1,"totalElements":2,"sort":null,"first":true,"numberOfElements":2,"size":20,"number":0}
+    $scope.create = function (appointment) {
+        $http.post("/appointments/create", appointment).success(function(){
+            $scope.appointments.push(angular.copy(appointment))
+        }).error(function(data){
+            window.alert("There was an error: "+data)
+        })
+
+    }
 
 });
