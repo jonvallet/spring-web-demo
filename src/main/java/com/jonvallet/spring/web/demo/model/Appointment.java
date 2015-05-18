bec4539436308acb9b6e0902916ecfb13d60a2b6
Jon Vallet <jon.vallet@proxama.com>
2015-05-18 15:31:56 +0100

    Added table name appointments

--- src/main/java/com/jonvallet/spring/web/demo/model/Appointment.java
+++ src/main/java/com/jonvallet/spring/web/demo/model/Appointment.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -12,6 +13,7 @@ import java.util.Date;
  * @author Jon Vallet
  */
 @Entity
+@Table(name = "Appointments")
 public class Appointment {
 
     @Id
